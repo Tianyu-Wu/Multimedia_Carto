265136a38cdc1db0b5edcad22ca8a0bb791df2ea
Tianyu Wu <t228wu@uwaterloo.ca>
2020-04-20 01:57:59 +0800

    atlas structure

---
+++ sustainable-mobility/src/main.js
@@ -0,0 +1,13 @@
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+
+Vue.config.productionTip = false;
+
+export const mapboxgl = window.mapboxgl;
+
+new Vue({
+  vuetify,
+  render: h => h(App)
+}).$mount("#app");
+
