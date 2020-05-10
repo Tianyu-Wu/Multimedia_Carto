12014b5099f13edfd93040c40240607964b77f6d
Tianyu Wu <t228wu@uwaterloo.ca>
2020-05-10 19:53:32 +0800

    all frequency maps

--- sustainable-mobility/src/components/CaseStudyMap.vue
+++ sustainable-mobility/src/components/CaseStudyMap.vue
@@ -1,22 +1,31 @@
 <template>
   <div>
     <!--<ZurichGreen  />-->
-    <SingaporeGreen />
+    <!-- <SingaporeGreen /> -->
     <!--<VancouverGreen  />-->
+    <VancouverFrequency />
+    <ZurichFrequency />
+    <SingaporeFrequency />
   </div>
 </template>
 
 <script>
 //import ZurichGreen from "./Green/Zurich-Green";
-import SingaporeGreen from "./Green/Singapore-Green";
+// import SingaporeGreen from "./Green/Singapore-Green";
 //import VancouverGreen from "./Green/Vancouver-Green";
+import VancouverFrequency from "./Frequency/Vancouver-Frequency";
+import ZurichFrequency from "./Frequency/Zurich-Frequency";
+import SingaporeFrequency from "./Frequency/Singapore-Frequency";
 
 export default {
   name: "CaseStudyMap",
   components: {
     //ZurichGreen
-    SingaporeGreen
+    // SingaporeGreen
     //VancouverGreen
+    VancouverFrequency,
+    ZurichFrequency,
+    SingaporeFrequency
   }
 };
 </script>
--- sustainable-mobility/src/components/Frequency/Vancouver-Frequency.vue
+++ sustainable-mobility/src/components/Frequency/Vancouver-Frequency.vue
@@ -10,18 +10,22 @@
     <v-row fluid no-gutters align="center">
       <v-col cols="12" md="8">
         <div id="van-frequency">Map</div>
+        <div class="legend-color"></div>
       </v-col>
       <v-col cols="6" md="4">
         <v-card class="d-flex flex-column justify-center" shaped height="600">
-          <v-card-text class="display-2 font-weight-bold"
-            >Frequency</v-card-text
-          >
+          <v-card-text class="display-2 font-weight-bold">Frequency</v-card-text>
           <div class="py-5"></div>
 
           <v-card-text>Some Stats Here.</v-card-text>
         </v-card>
       </v-col>
     </v-row>
+    <!-- <v-row>
+      <v-col>
+        <div class="legend-color"></div>
+      </v-col>
+    </v-row>-->
   </div>
 </template>
 
@@ -38,22 +42,52 @@ export default {
       let self = this;
       self.map = new mapboxgl.Map({
         container: "van-frequency",
-        style: "mapbox://styles/mmcartog01/ck98marof00vr1io3vqd4ehlb/draft",
+        style: "mapbox://styles/mapbox/light-v10",
+        // style: "mapbox://styles/mmcartog01/ck98marof00vr1io3vqd4ehlb/draft",
         center: [-122.949603, 49.224873],
         zoom: 9.5
       });
 
-      // self.map.on("load", function() {
-      //   self.map.addLayer({
-      //     id: "vancouver-boarding",
-      //     type: "circle",
-      //     source: {
-      //       type: "vector",
-      //       url: "mapbox://mmcartog01.0tmwze2x"
-      //     },
-      //     "source-layer": "vancouver_boarding-dyq3tk"
-      //   });
-      // });
+      self.map.on("load", function() {
+        self.map.addLayer({
+          id: "vancouver-boarding",
+          type: "circle",
+          source: {
+            type: "vector",
+            url: "mapbox://mmcartog01.0tmwze2x"
+          },
+          "source-layer": "vancouver_boarding-dyq3tk",
+          paint: {
+            "circle-radius": [
+              "interpolate",
+              ["linear"],
+              ["get", "avg_daily_boarding_mf"],
+              0,
+              3,
+              5000,
+              5,
+              10000,
+              10
+            ],
+            "circle-color": [
+              "interpolate",
+              ["linear"],
+              ["get", "avg_daily_boarding_mf"],
+              0,
+              "#8ab9d6",
+              2500,
+              "#fed56c",
+              5000,
+              "#f09f66",
+              7500,
+              "#f2683a",
+              10000,
+              "#f75555"
+            ],
+            "circle-opacity": 0.8
+          }
+        });
+      });
     }
   },
   mounted() {
@@ -67,4 +101,19 @@ export default {
   width: 100%;
   height: 600px;
 }
+.legend-color {
+  height: 12px;
+  background: linear-gradient(
+    to right,
+    #8ab9d6,
+    #fed56c,
+    #f09f66,
+    #f2683a,
+    #f75555
+  );
+}
 </style>
+
+
+
+
