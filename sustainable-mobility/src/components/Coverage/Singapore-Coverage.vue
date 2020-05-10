99dcbeea9a5d6ee18aabae8b62362864a5b2ef91
Yihang She <yihshe@student.ethz.ch>
2020-05-10 17:07:25 +0800

    Yihang She, tansportation coverage and global GHG map

---
+++ sustainable-mobility/src/components/Coverage/Singapore-Coverage.vue
@@ -0,0 +1,63 @@
+<template>
+  <div fill-height color="#F3EBE3">
+    <v-container>
+      <div class="py-12"></div>
+
+      <h2 class="display-3 font-weight-bold mb-3">Singapore - Asia</h2>
+      <div class="py-5"></div>
+    </v-container>
+
+    <v-row fluid no-gutters align="center">
+      <v-col cols="12" md="8">
+        <div id="sing-coverage">Map</div>
+      </v-col>
+      <v-col cols="6" md="4">
+        <v-card class="d-flex flex-column justify-center" shaped height="600">
+          <v-card-text class="display-2 font-weight-bold"
+            >Coverage</v-card-text
+          >
+          <div class="py-5"></div>
+
+          <v-card-text>There transport coverage over the Singapore island is generally high, and there 
+              are also multiple transportation centers, which is kind of similar with Zurich. The high 
+              coverage of public transportation also results from the high population densities in this 
+              city-state, which is also a common feature in many Asian countries.
+        </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import { mapboxgl } from "@/main";
+
+export default {
+  name: "SingaporeCoverage",
+  data: () => ({
+    map: null
+  }),
+  methods: {
+    initMap: function() {
+      let self = this;
+      self.map = new mapboxgl.Map({
+        container: "sing-coverage",
+        style: "mapbox://styles/mmcartog01/ck9tms1dv0mtl1ipiy8fkgxsa",
+        center: [103.824, 1.363],
+        zoom: 10.0
+      });
+
+    }
+  },
+  mounted() {
+    this.initMap();
+  }
+};
+</script>
+
+<style>
+#sing-coverage {
+  width: 100%;
+  height: 600px;
+}
+</style>
---
+++ sustainable-mobility/src/components/Coverage/Zurich-Coverage.vue
@@ -0,0 +1,62 @@
+<template>
+  <div fill-height color="#F3EBE3">
+    <v-container>
+      <div class="py-12"></div>
+
+      <h2 class="display-3 font-weight-bold mb-3">Zurich - Europe</h2>
+      <div class="py-5"></div>
+    </v-container>
+
+    <v-row fluid no-gutters align="center">
+      <v-col cols="12" md="8">
+        <div id="zuri-coverage">Map</div>
+      </v-col>
+      <v-col cols="6" md="4">
+        <v-card class="d-flex flex-column justify-center" shaped height="600">
+          <v-card-text class="display-2 font-weight-bold"
+            >Coverage</v-card-text
+          >
+          <div class="py-5"></div>
+
+          <v-card-text>The public transport in Zurich covers almost every place
+              of the city with a similar density. The kernel density of stations in 
+              the downtown is slightly higher than the remaining area, and There are 
+              multiple transport centers in Zurich.</v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import { mapboxgl } from "@/main";
+
+export default {
+  name: "ZurichCoverage",
+  data: () => ({
+    map: null
+  }),
+  methods: {
+    initMap: function() {
+      let self = this;
+      self.map = new mapboxgl.Map({
+        container: "zuri-coverage",
+        style: "mapbox://styles/mmcartog01/ck9tms1dv0mtl1ipiy8fkgxsa",
+        center: [8.551, 47.378],
+        zoom: 11.0
+      });
+
+    }
+  },
+  mounted() {
+    this.initMap();
+  }
+};
+</script>
+
+<style>
+#zuri-coverage {
+  width: 100%;
+  height: 600px;
+}
+</style>
